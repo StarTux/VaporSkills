1cbed4d2d170898c123b7cd545674c863365ffa3
Thomas Loy <ths.loy@googlemail.com>
2015-07-22 12:00:04 +0200

    Override abstract methods

--- src/main/java/com/winthier/skills/bukkit/BukkitSkillArcher.java
+++ src/main/java/com/winthier/skills/bukkit/BukkitSkillArcher.java
@@ -13,10 +13,38 @@ class BukkitSkillArcher extends BukkitSkillAbstractEntityKill
     final String verb = "snipe";
     final String personName = "archer";
     final String activityName = "archery";
+    final double KILL_DISTANCE = 8;
+    final long KILL_DISTANCE_SECONDS = 60L * 5;
+    final double PLAYER_DISTANCE = 16;
 
     @Override
     boolean allowDamager(Entity entity, Player player)
     {
         return entity instanceof Arrow;
+    }
+
+    @Override
+    boolean useKillDistance()
+    {
+        return true;
+    }
+
+    @Override
+    double fullKillDistance()
+    {
+        return KILL_DISTANCE;
+    }
+
+    @Override
+    long killDistanceSeconds()
+    {
+        return KILL_DISTANCE_SECONDS;
+    }
+
+    @Override
+    double scoreMultiplier(Player player, Entity entity)
+    {
+        double distance = player.getLocation().distance(entity.getLocation());
+        return Math.min(1.0, distance / PLAYER_DISTANCE);
     }
 }
