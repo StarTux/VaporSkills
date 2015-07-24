fa137faee4df20655aefb086f000a7707d129e76
Thomas Loy <ths.loy@googlemail.com>
2015-07-24 11:54:04 +0200

    Reward crops broken by water

--- src/main/java/com/winthier/skills/bukkit/BukkitSkillHarvest.java
+++ src/main/java/com/winthier/skills/bukkit/BukkitSkillHarvest.java
@@ -1,6 +1,13 @@
 package com.winthier.skills.bukkit;
 
+import com.winthier.skills.Reward;
 import lombok.Getter;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.block.BlockFromToEvent;
 
 @Getter
 class BukkitSkillHarvest extends BukkitSkillAbstractBlockBreak
@@ -10,11 +17,26 @@ class BukkitSkillHarvest extends BukkitSkillAbstractBlockBreak
     final String verb = "harvest";
     final String personName = "harvester";
     final String activityName = "harvesting";
+    final double RADIUS = 40;
 
     @Override
     Boolean requirePlayerPlacedBlock()
     {
         // We want blocks to be *not* player placed.
 	return false;
+    }
+
+    @EventHandler(ignoreCancelled=true, priority=EventPriority.MONITOR)
+    public void onBlockFromTo(BlockFromToEvent event) {
+        // In addition to block breaks, the Harvest Skill also
+        // rewards crops broken by water.
+        Block source = event.getBlock();
+        if (source.getType() != Material.WATER && source.getType() != Material.STATIONARY_WATER) return;
+        Block block = event.getToBlock();
+        Reward reward = rewardForBlock(block);
+        if (reward == null) return;
+        Player player = getNearestPlayer(block.getLocation(), RADIUS);
+        if (player == null) return;
+        giveReward(player, reward);
     }
 }
