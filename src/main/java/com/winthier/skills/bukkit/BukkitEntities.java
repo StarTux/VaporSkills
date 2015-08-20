40700c9e314bf48eb0d31b5405e925a39bcb05d9
Thomas Loy <ths.loy@googlemail.com>
2015-08-20 22:31:34 +0200

    Add BabyZombie

--- src/main/java/com/winthier/skills/bukkit/BukkitEntities.java
+++ src/main/java/com/winthier/skills/bukkit/BukkitEntities.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Skeleton;
+import org.bukkit.entity.Zombie;
 
 // TODO: Check special names
 class BukkitEntities
@@ -13,6 +14,7 @@ class BukkitEntities
     private static List<String> entityNames;
     static enum SpecialNames {
         WITHER_SKELETON("WitherSkeleton"),
+        BABY_ZOMBIE("BabyZombie"),
         ;
         final String name;
         SpecialNames(String name) { this.name = name; }
@@ -31,6 +33,7 @@ class BukkitEntities
     static String name(Entity e)
     {
         if (e instanceof Skeleton && ((Skeleton)e).getSkeletonType() == Skeleton.SkeletonType.WITHER) return SpecialNames.WITHER_SKELETON.name;
+        if (e instanceof Zombie && ((Zombie)e).isBaby()) return SpecialNames.BABY_ZOMBIE.name;
         return name(e.getType());
     }
 
