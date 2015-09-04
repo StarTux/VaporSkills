993b7276354419c453b1def2c118c6d27a737ec2
Thomas Loy <ths.loy@googlemail.com>
2015-09-04 14:55:55 +0200

    Fix Enchanting skill. Note for later: EnchantItemEvent.getExpLevelCost() does not yield the actual level cost, but the required levels. Must be a legacy from before 1.8 was released.

--- src/main/java/com/winthier/skills/bukkit/BukkitSkillEnchant.java
+++ src/main/java/com/winthier/skills/bukkit/BukkitSkillEnchant.java
@@ -1,13 +1,12 @@
 package com.winthier.skills.bukkit;
 
-import java.util.Map;
 import lombok.Getter;
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.enchantment.EnchantItemEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 class BukkitSkillEnchant extends BukkitSkill implements Listener
 {
@@ -16,9 +15,19 @@ class BukkitSkillEnchant extends BukkitSkill implements Listener
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     public void onEnchantItem(EnchantItemEvent event)
     {
-        Player player = event.getEnchanter();
+        final Player player = event.getEnchanter();
         if (player == null) return;
         if (!allowPlayer(player)) return;
-        giveReward(player, rewardForName("exp_level_cost", event.getExpLevelCost()));
+        final int level = player.getLevel();
+        new BukkitRunnable() {
+            @Override public void run() {
+                onEnchanted(player, level);
+            }
+        }.runTask(getPlugin());
+    }
+
+    void onEnchanted(Player player, int oldLevel) {
+        int levelsSpent = oldLevel - player.getLevel();
+        giveReward(player, rewardForName("exp_level_cost", levelsSpent));
     }
 }
