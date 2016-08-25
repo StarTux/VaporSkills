472ff13620e0560c22262262e71aa6a5a147926e
Thomas Loy <ths.loy@googlemail.com>
2016-08-25 14:37:56 +0200

    Digging and Woodcutting does not care about tools

--- src/main/java/com/winthier/skills/bukkit/BukkitSkillDig.java
+++ src/main/java/com/winthier/skills/bukkit/BukkitSkillDig.java
@@ -7,20 +7,4 @@ import org.bukkit.inventory.ItemStack;
 class BukkitSkillDig extends BukkitSkillAbstractBlockBreak
 {
     @lombok.Getter final BukkitSkillType skillType = BukkitSkillType.DIG;
-
-    @Override
-    boolean allowItemInHand(ItemStack item)
-    {
-	// Any shovel goes
-	switch (item.getType()) {
-	case DIAMOND_SPADE:
-	case GOLD_SPADE:
-	case IRON_SPADE:
-	case STONE_SPADE:
-	case WOOD_SPADE:
-	    return true;
-	default:
-	    return false;
-	}
-    }
 }
--- src/main/java/com/winthier/skills/bukkit/BukkitSkillWoodcut.java
+++ src/main/java/com/winthier/skills/bukkit/BukkitSkillWoodcut.java
@@ -7,20 +7,4 @@ import org.bukkit.inventory.ItemStack;
 class BukkitSkillWoodcut extends BukkitSkillAbstractBlockBreak
 {
     @Getter final BukkitSkillType skillType = BukkitSkillType.WOODCUT;
-
-    @Override
-    boolean allowItemInHand(ItemStack item)
-    {
-	// Any pickaxe goes
-	switch (item.getType()) {
-	case DIAMOND_AXE:
-	case GOLD_AXE:
-	case IRON_AXE:
-	case STONE_AXE:
-	case WOOD_AXE:
-	    return true;
-	default:
-	    return false;
-	}
-    }
 }
