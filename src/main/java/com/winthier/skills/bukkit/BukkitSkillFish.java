058dbb019ac20dfbde12c19bd2d753dd7af62765
StarTux <ths.loy@googlemail.com>
2017-04-17 13:56:32 +0200

    Protect against afk fishing

--- src/main/java/com/winthier/skills/bukkit/BukkitSkillFish.java
+++ src/main/java/com/winthier/skills/bukkit/BukkitSkillFish.java
@@ -1,5 +1,6 @@
 package com.winthier.skills.bukkit;
 
+import com.winthier.exploits.bukkit.BukkitExploits;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -11,16 +12,23 @@ import org.bukkit.inventory.ItemStack;
 class BukkitSkillFish extends BukkitSkill implements Listener
 {
     @lombok.Getter final BukkitSkillType skillType = BukkitSkillType.FISH;
+    private long fishInterval = 15;
 
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+    @Override
+    public void configure() {
+        super.configure();
+        fishInterval = getConfig().getLong("FishInterval", fishInterval);
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
     public void onPlayerFish(PlayerFishEvent event)
     {
-        // TODO: Consider afk fishing exploit
         if (event.getState() != PlayerFishEvent.State.CAUGHT_FISH) return;
         Player player = event.getPlayer();
         if (player == null) return;
         if (!allowPlayer(player)) return;
         if (!(event.getCaught() instanceof Item)) return;
+        if (BukkitExploits.getInstance().didRecentlyFish(player, event.getCaught().getLocation().getBlock(), fishInterval)) return;
         ItemStack item = ((Item)event.getCaught()).getItemStack();
         giveReward(player, rewardForItem(item));
     }
