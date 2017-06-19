db99044e54f2054eb6aaa724d86b1aae93191a48
StarTux <ths.loy@googlemail.com>
2017-06-19 10:00:55 +0200

    Clean up entity naming

--- src/main/java/com/winthier/skills/bukkit/BukkitEntities.java
+++ src/main/java/com/winthier/skills/bukkit/BukkitEntities.java
@@ -6,19 +6,11 @@ import java.util.List;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.MagmaCube;
-import org.bukkit.entity.PigZombie;
-import org.bukkit.entity.Skeleton;
 import org.bukkit.entity.Slime;
-import org.bukkit.entity.Zombie;
 
-// TODO: Check special names
-class BukkitEntities
-{
+final class BukkitEntities {
     private static List<String> entityNames;
     static enum SpecialNames {
-        WITHER_SKELETON("WitherSkeleton"),
-        BABY_ZOMBIE("BabyZombie"),
-        BABY_PIG_ZOMBIE("BabyPigZombie"),
         BIG_SLIME("BigSlime"),
         SMALL_SLIME("SmallSlime"),
         TINY_SLIME("TinySlime"),
@@ -30,8 +22,7 @@ class BukkitEntities
         SpecialNames(String name) { this.name = name; }
     }
 
-    static List<String> getEntityNames()
-    {
+    static List<String> getEntityNames() {
         if (entityNames == null) {
             entityNames = new ArrayList<>();
             for (SpecialNames name : SpecialNames.values()) entityNames.add(name.name);
@@ -39,26 +30,26 @@ class BukkitEntities
         }
         return entityNames;
     }
-    
-    static String name(Entity e)
-    {
+
+    static String name(Entity e) {
         EntityType type = e.getType();
-        if (type == EntityType.SKELETON && e instanceof Skeleton && ((Skeleton)e).getSkeletonType() == Skeleton.SkeletonType.WITHER) return SpecialNames.WITHER_SKELETON.name;
-        if (type == EntityType.PIG_ZOMBIE && e instanceof PigZombie && ((PigZombie)e).isBaby()) return SpecialNames.BABY_ZOMBIE.name;
-        if (type == EntityType.ZOMBIE && e instanceof Zombie && ((Zombie)e).isBaby()) return SpecialNames.BABY_PIG_ZOMBIE.name;
-        if (type == EntityType.SLIME && e instanceof Slime) {
+        switch (e.getType()) {
+        case SLIME:
             switch (((Slime)e).getSize()) {
             case 4: return SpecialNames.BIG_SLIME.name;
             case 2: return SpecialNames.SMALL_SLIME.name;
             case 1: return SpecialNames.TINY_SLIME.name;
+            default: break;
             }
-        }
-        if (type == EntityType.MAGMA_CUBE && e instanceof Slime) {
+            break;
+        case MAGMA_CUBE:
             switch (((Slime)e).getSize()) {
             case 4: return SpecialNames.BIG_MAGMA_CUBE.name;
             case 2: return SpecialNames.SMALL_MAGMA_CUBE.name;
             case 1: return SpecialNames.TINY_MAGMA_CUBE.name;
+            default: break;
             }
+        default: break;
         }
         return name(e.getType());
     }
