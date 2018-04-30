7353db0d7328cd0b67ad450b4934bd6e128e13be
StarTux <ths.loy@googlemail.com>
2018-04-30 17:51:25 +0200

    Begin overhaul

--- src/main/java/com/winthier/skills/RanchSkill.java
+++ src/main/java/com/winthier/skills/RanchSkill.java
@@ -1,24 +1,72 @@
 package com.winthier.skills;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntityBreedEvent;
 
 final class RanchSkill extends Skill implements Listener {
-    static final double RADIUS = 10.0;
+    static final String BREED_KEY = "winthier.skill.Ranch.breed";
+
+    private static class Breed {
+        int generation;
+        UUID owner;
+        List<String> quirks;
+        List<String> hiddenQuirks;
+    }
 
     RanchSkill(SkillsPlugin plugin) {
         super(plugin, SkillType.RANCH);
     }
 
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
-    public void onCreatureSpawn(CreatureSpawnEvent event) {
-        if (event.getSpawnReason() != CreatureSpawnEvent.SpawnReason.BREEDING) return;
-        Player player = getNearestPlayer(event.getEntity(), RADIUS);
-        if (player == null) return;
-        if (!allowPlayer(player)) return;
-        giveReward(player, rewardForEntity(event.getEntity()));
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGH)
+    public void onEntityBreed(EntityBreedEvent event) {
+        if (!(event.getBreeder() instanceof Player)) return;
+        final Player player = (Player)event.getBreeder();
+        final LivingEntity entity = event.getEntity();
+        giveReward(player, rewardForEntity(entity));
+        onBreed(player, event.getMother(), event.getFather(), entity);
+    }
+
+    static Map<String, Object> getStoredData(LivingEntity e, String key) {
+        for (String s: e.getScoreboardTags()) {
+            if (s.startsWith(key)) {
+                String v = s.substring(key.length());
+                Object o = Msg.fromJSONString(v);
+                if (!(o instanceof Map)) return null;
+                @SuppressWarnings("unchecked")
+                Map<String, Object> result = (Map<String, Object>)o;
+                return result;
+            }
+        }
+        return null;
+    }
+
+    static int removeStoredData(LivingEntity e, String key) {
+        List<String> rs = new ArrayList<>();
+        for (String s: e.getScoreboardTags()) {
+            if (s.startsWith(key)) rs.add(s);
+        }
+        for (String r: rs) {
+            e.removeScoreboardTag(r);
+        }
+        return rs.size();
+    }
+
+    static void storeData(LivingEntity e, String key, Map<String, Object> d) {
+        removeStoredData(e, key);
+        e.addScoreboardTag(key + Msg.toJSONString(d));
+    }
+
+    private void onBreed(Player player, LivingEntity mother, LivingEntity father, LivingEntity baby) {
     }
 }
